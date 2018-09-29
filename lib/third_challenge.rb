04e59f16a98f220512586fa9e3e0133c79288192
David Sim <wsim903@gmail.com>
2018-09-29 20:15:33 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,25 +2,33 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        "name", 
+        "age"
         },
       :matriarch => {
-        #your key/value pairs here
+        "name", 
+        "age"
         },
       :hero => {
-        #your key/value pairs here
+        "name", 
+        "age",
+        "status"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        "name", 
+        "age"
         },
       :matriarch => {
-        #your key/value pairs here
+        "name", 
+        "age"
         },
       :heroine => {
-        #your key/value pairs here
+        "name", 
+        "age",
+        "status"
         },
       :heroine_friends => []
    }
