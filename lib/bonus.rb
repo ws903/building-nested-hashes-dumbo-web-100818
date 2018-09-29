7f7f234487186186caef19faf1ba3fe6de1ec9d9
David Sim <wsim903@gmail.com>
2018-09-29 20:21:30 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,8 @@ def bonus
    }
   }
   
-  bonus["montague"]["hero"]["status"] = "dead"
-  bonus[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   
 
