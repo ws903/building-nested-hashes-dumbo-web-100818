1bea3740534c344fcaa95e15f4cac245527cc128
David Sim <wsim903@gmail.com>
2018-09-29 20:13:35 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      "patriarch", 
+      "patriarch":, 
       "matriarch", 
       "hero", 
       "hero_friends"
       }, 
     :capulet => {
-      #your key/value pairs here!
+      "patriarch", 
+      "matriarch",
+      "heroine", 
+      "heroine_friends"
       }}
 
   
